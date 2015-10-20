3f8480162be67af767787216796d8c267a527456
OnOn_uestc <413972057@qq.com>
2015-10-20 16:54:37 +0800

    剑阁县委组织部”12380“举报网站前端V2.0

---
+++ js/close_window.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Administrator on 2015/10/20.
+ */
+function CloseWebPage(){
+    if (navigator.userAgent.indexOf("MSIE") > 0) {
+        if (navigator.userAgent.indexOf("MSIE 6.0") > 0) {
+            window.opener = null;
+            window.close();
+        } else {
+            window.open('', '_top');
+            window.top.close();
+        }
+    }
+    else if (navigator.userAgent.indexOf("Firefox") > 0) {
+        window.location.href = 'about:blank ';
+    } else {
+        window.opener = null;
+        window.open('', '_self', '');
+        window.close();
+    }
+}
+
+function print_content(){
+    $("#main_rules_content").jqprint();
+}
